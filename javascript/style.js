56c4fc401435d44381c57d9a65621a32a5dbbb8c
lawrenzo001 <oznerwins@gmail.com>
2023-09-03 21:09:32 +0100

    Responsive on 1440px

--- javascript/style.js
+++ javascript/style.js
@@ -1,66 +1,6 @@
 
-// .slider-container {
-//     width: 100%;
-//     overflow: hidden;
-// }
-
-// .slide {
-//     display: none;
-// }
-
-// .slide img {
-//     width: 100%;
-//     height: auto;
-// }
-
-
-// let slideindex = 1;
-// showSlides(slideindex);
-
-
-// function plusSlides(n) {
-//     showSlides(slideindex += n);
-
-// }
-
-// function currentSlide(n) {
-//     showSlides(slideindex = n);
-// }
-
-// function showSlides(n) {
-//     let i;
-//     let slides = document.getElementsByClassName("slide");
-//     let dots = document.getElementsByClassName("dot");
-
-//     if (n > slideindex.length) {
-//         slideindex = 1;
-
-//     }
-
-//     if (n < 1) {
-//         slideindex = slides.length;
-
-//     }
-
-//     for (i = 0; i < dots.length; i++) {
-//         dots[i].style.display = "none";
-//     }
-    
-//     for (i = 0; i < dots.length; i++) {
-//         dots[i].classList.remove = "active";
-//     }
-
-//     slides[slideindex - 1].style.display = "block";
-//     dots[slideindex - 1].classList.add("active");
-
-
-// }
-
-
-
 const optionMenu1 = document.querySelector(".dropdwn1"),
         selectBtn1 = optionMenu1.querySelector(".drop-btn1"),
-        // options = optionMenu.querySelectorAll(".option"),
         sBtn_text1 = optionMenu1.querySelector(".drop-text1");
 
 selectBtn1.addEventListener("click", () => optionMenu1.classList.toggle("active"));
@@ -69,7 +9,6 @@ selectBtn1.addEventListener("click", () => optionMenu1.classList.toggle("active"
 
 const optionMenu2 = document.querySelector(".dropdwn2"),
         selectBtn2 = optionMenu2.querySelector(".drop-btn2"),
-        // options = optionMenu.querySelectorAll(".option"),
         sBtn_text2 = optionMenu2.querySelector(".drop-text2");
 
 
@@ -79,7 +18,6 @@ selectBtn2.addEventListener("click", () => optionMenu2.classList.toggle("active"
 
 const optionMenu3 = document.querySelector(".dropdwn3"),
         selectBtn3 = optionMenu3.querySelector(".drop-btn3"),
-        // options = optionMenu.querySelectorAll(".option"),
         sBtn_text3 = optionMenu3.querySelector(".drop-text3");
 
 
@@ -89,7 +27,6 @@ selectBtn3.addEventListener("click", () => optionMenu3.classList.toggle("active"
 
 const optionMenu4 = document.querySelector(".dropdwn4"),
         selectBtn4 = optionMenu4.querySelector(".drop-btn4"),
-        // options = optionMenu.querySelectorAll(".option"),
         sBtn_text4 = optionMenu4.querySelector(".drop-text4");
 
 
@@ -111,13 +48,41 @@ const swiper = new Swiper('.sample-slider', {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+
+    breakpoints:{
+        2560: {
+            width: 2560,
+            slidesPerView: 4,
+        },
+
+        1440: {
+            width: 1440,
+            slidesPerView: 3.5,
+        },
+
+        // 520: {
+        //     slidesPerView: 2;
+        // },
+
+        // 950: {
+        //     slidesPerView: 3;
+        // },
+
+        // 1024: {
+        //     slidesPerView: 3;
+        // },
+
+        // 1440: {
+        //     slidesPerView: 3;
+        // },
+    },
 })
 
 
 
-const swiper1 = new Swiper('.slider1', {
+const swiper1 = new Swiper('.sample-slider1', {
         slidesPerView: 3,
-        spaceBetween: 40,
+        spaceBetween: 20,
         loop: true,                         //loop
         autoplay: {                         //autoplay
             delay: 2000,  
@@ -130,36 +95,5 @@ const swiper1 = new Swiper('.slider1', {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         },
-    })
+})
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var swiper = new Swiper(".sample-slider1", {
-//         slidesPerView: 4,
-//         spaceBetween: 20,
-//         loop: true,
-//         pagination: {
-//           el: ".swiper-pagination",
-//           clickable: true,
-//         },
-//         navigation: {
-//           nextEl: ".swiper-button-next",
-//           prevEl: ".swiper-button-prev",
-//         },
-//     });
-        
